97b59359c2f0962189026c2bf5cd4c492a472a6d
SUNDARAVALLIS <113921048+SUNDARAVALLIS@users.noreply.github.com>
2024-08-21 10:22:37 +0530

    Add files via upload

---
+++ javascript/demo.js
@@ -0,0 +1,139 @@
+// var a=10
+// var b=10
+// console.log(a+b)
+
+/*function iphone(){
+    console.log("this is iphone")
+}
+iphone()
+
+function redmi(){
+    console.log("this is redmi")
+}
+redmi()
+
+function oppo(){
+    console.log("this is oppo")
+}
+oppo()*/
+
+//functions:
+// var factor="kamal"
+// var fmovie="gilli"
+// var fsong="shala la"
+// function favourite(){
+//     console.log("fav actor:"+factor)
+//     console.log("fav movie:"+fmovie)
+//     console.log("fav song:"+fsong)
+// }
+// favourite()
+
+
+//parameters:
+// var l=10
+// var b=20
+// function area(length,breadth){
+//     console.log(length*breadth)
+// }
+// area(l,b)
+
+//return
+// var a=10
+// var b=20
+// function add(){
+//     return (a+b)
+// }
+// var c=add()
+// console.log(c)
+
+
+//if-else
+// var rain=false  
+// if(rain){  //works for true
+//     console.log("Enjoy the rain")
+// }else{ //works for false
+//     console.log("enjoy the sunshine")
+// }
+
+// var season="spring"
+// if(season=="spring")
+// {
+//     console.log("enjoy flowers")
+// }
+// if(season=="summer")
+// {
+//     console.log("dont go out")
+// }
+// if(season=="autum")
+// {
+//     console.log("colourful leaves")
+// }
+
+// if(season=="winter"){
+//     console.log("winter joy")
+// }
+
+// var char='a'
+// if(char='a'||'e'||'i'||'o'||'u'){
+//     console.log("its vowel")
+// }else{
+//     console.log("its consonant")
+// }
+
+
+// odd or even
+// var a=20;
+// if(a%2==0){
+//     console.log("its even")
+// }else{
+//     console.log("its odd")
+// }
+
+
+//print from 1 to 10 using for loop
+// for(i=1;i<=10;i++){
+    //  console.log(i) 
+//     console.log("sundaravalli")
+// }
+
+
+//print from 1 to 10 by increment of 2using for loop
+// for(i=1;i<=10;i=i+2){
+//      console.log(i) 
+//     // consnodeole.log("sundaravalli")
+// }
+
+//print from 1 to 10 in reverse using for loop
+// for(i=10;i>1;i--){
+//     console.log(i) 
+//    // consnodeole.log("sundaravalli")
+// }
+
+
+//print only even numbers from 1 to 20
+// for(i=0;i<=20;i++){
+//     if(i%2==0){
+//         console.log(i)
+//     }
+// }
+
+
+// Display the multiplication table for 2
+// console.log("Multiplication Table for 2:");
+
+// for (let i = 1; i <= 10; i++) {
+//     let result = 2 * i;
+//     console.log(`2 x ${i} = ${result}`);
+// }
+
+
+//array operations
+// var fruits=["apple","mango","banana","kiwi"]
+// console.log(fruits[0])
+
+// var fruits=["apple","mango","banana","kiwi"]
+// console.log(fruits.length)
+// for(count=1;count<fruits.length;count=count+1){
+//     console.log(fruits[count])
+// }
+
